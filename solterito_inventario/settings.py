b1d71576f9ce4ee4c50ff0b065698a7b7dc6adb2
burggos <gabrielbucarra17@gmail.com>
2026-05-11 09:49:59 -0500

    errores 500 en render.com al intentar desplegar la aplicación. Se han actualizado las dependencias en requirements.txt y se ha configurado la variable de entorno PYTHON_VERSION en render.yaml para asegurar la compatibilidad con Python 3.11.9.

--- solterito_inventario/settings.py
+++ solterito_inventario/settings.py
@@ -138,6 +138,38 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(name)s %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.environ.get('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.server': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
 
 # Seguridad de despliegue (solo producción)
 if IS_PRODUCTION:
